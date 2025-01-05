504544fca03dfd69a823cc4e5d7a6125728224aa
MOCH. RIZKI KURNIAWAN <92960397+Mochrks@users.noreply.github.com>
2025-01-05 10:55:41 +0700

    fix :cpoyright yeears

--- components/demo/Footer.tsx
+++ components/demo/Footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
-
+    const getFullyears = new Date().getFullYear();
     return (
         <footer className="border-t">
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-center items-center">
                     <div className="">
-                        © 2024 All rights reserved.  <a
+                        © {getFullyears} All rights reserved.  <a
                             href="https://github.com/mochrks"
                             target="_blank"
                             rel="noopener noreferrer"
